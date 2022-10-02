125889d10e3f5a7e5c9a248b9be2e762e1651ec3
Valentyn Musiienko <musiienkovalentyn@gmail.com>
2022-10-02 22:56:45 +0300

    10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  widgetText: document.querySelector(".color"),
+  widgetBtn: document.querySelector(".change-color"),
+};
+
+refs.widgetBtn.addEventListener("click", onClickChangeBgc);
+
+function onClickChangeBgc(event) {
+  refs.widgetText.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  collection: document.querySelector("#boxes"),
+  crateBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  inputField: document.querySelector('[type="number"]'),
+};
+
+refs.crateBtn.addEventListener("click", onCreateBtnClickMasteringCollection);
+refs.destroyBtn.addEventListener("click", onDestroyBtnClickDestroyCollection);
+
+function onCreateBtnClickMasteringCollection(amount) {
+  amount = refs.inputField.value;
+  for (let i = 0; i < amount; i += 1) {
+    const collectionElement = document.createElement("div");
+    collectionElement.setAttribute("width", `${30 + i * 10}px`);
+    collectionElement.setAttribute("hight", `${30 + i * 10}px`);
+    collectionElement.style.backgroundColor = getRandomHexColor();
+    refs.collection.append(collectionElement);
+  }
+}
+
+function onDestroyBtnClickDestroyCollection() {
+  refs.collection.innerHTML = "";
+}
